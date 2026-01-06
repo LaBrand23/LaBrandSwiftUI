e8120d5c3ce915eb4f55fd7208ee18372bce829f
Shakhzod <shaxzodsunnatov23@gmail.com>
2026-01-06 11:12:44 +0500

    Firebase Connected+

--- LaBrandSwiftUI/LaBrandSwiftUIApp.swift
+++ LaBrandSwiftUI/LaBrandSwiftUIApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
+// MARK: - App Delegate for Firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+// MARK: - Main App
 @main
 struct LaBrandSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     @StateObject private var authManager = AuthenticationManager()
     @StateObject private var themeManager = ThemeManager.shared
     
